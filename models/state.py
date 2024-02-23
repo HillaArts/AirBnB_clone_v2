5df03b250541563da3f54d93e5fdf890c243167b
HillaArts <hassenyehillary@gmail.com>
2024-02-23 06:21:35 -0800

    change your storage engine and use SQLAlchemy

--- models/state.py
+++ models/state.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
+class State(BaseModel, Base):
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+    @property
+    def cities(self):
+        """FileStorage relationship between State and City."""
+        from models import storage
+        all_cities = storage.all(City)
+        return [city for city in all_cities.values() if city.state_id == self.id]
