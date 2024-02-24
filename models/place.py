dcfffbfc9400a87aad7fdb84c900596d94b0c3a4
HillaArts <hassenyehillary@gmail.com>
2024-02-24 11:54:02 -0800

    update

--- models/place.py
+++ models/place.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy import Table, Column, String, ForeignKey, Integer
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
+# Association table for the Many-To-Many relationship between Place and Amenity
+place_amenity = Table('place_amenity', Base.metadata,
+    Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+    Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)
+)
 
-class Place(BaseModel):
-    """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+class Place(BaseModel, Base):
+    __tablename__ = 'places'
+    # Assuming other attributes of the Place model are defined here
+    amenities = relationship("Amenity", secondary=place_amenity, back_populates="place_amenities", viewonly=False)
+
+    @property
+    def amenities(self):
+        """FileStorage: Getter attribute amenities that returns the list of Amenity instances"""
+        from models import storage
+        return [amenity for amenity in storage.all(Amenity).values() if amenity.id in self.amenity_ids]
+
+    @amenities.setter
+    def amenities(self, obj):
+        """FileStorage: Setter attribute amenities that adds an Amenity.id to the amenity_ids"""
+        if type(obj).__name__ == 'Amenity':
+            self.amenity_ids.append(obj.id)
+
--- models/user.py
+++ models/user.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel, Base  # Assuming Base is also imported here
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes for DB storage"""
     __tablename__ = 'users'
+    # Assuming other user attributes are defined here.
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    # Relationship with reviews
+    reviews = relationship("Review", back_populates="user", cascade="all, delete-orphan")
 
